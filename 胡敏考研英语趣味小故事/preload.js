38b4ae082bbcba08dd48927fab083b7c9a5d708f
Lorie Kautzer <143466086+yuzonghui@users.noreply.github.com>
2026-06-05 15:15:32 +0800

    Delete 胡敏考研英语趣味小故事 directory

--- 胡敏考研英语趣味小故事/preload.js
+++
@@ -1,7 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('api', {
-  loadUserData: () => ipcRenderer.invoke('load-user-data'),
-  saveUserData: (data) => ipcRenderer.invoke('save-user-data', data),
-  loadStories: () => ipcRenderer.invoke('load-stories')
-});
